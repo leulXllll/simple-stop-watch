f1a1c83077442d9a802d716cc3e931d2d9e707d0
Leul Zewdu <Leulzewduchanie@gmail.com>
2024-12-08 09:59:56 +0300

    fixed some bug when changing to hour

--- src/Stopwatch.jsx
+++ src/Stopwatch.jsx
@@ -12,6 +12,7 @@ const Stopwatch = () => {
     const [value,setValue] =useState(false);
 
     const secref = useRef(0);
+    const minref = useRef(0);
 
     useEffect(()=>{
         if(value){
@@ -19,14 +20,17 @@ const Stopwatch = () => {
             let interval = setInterval(()=>{
                 
                 secref.current = sec;
-                if(secref.current==60){
-                    setMin(min+1);
-                    setSec(0);
-                }else if(min==60){
+
+                minref.current = min;
+
+                if(minref.current==59 && secref.current==59){
                     setHour(hour+1);
                     setSec(0);
                     setMin(0);
-                }
+                }else if(secref.current==59){
+                    setMin(min+1);
+                    setSec(0);
+                } 
                 
                 setSec(sec=>sec+1);
                 

d7e2ebf9c5d5c8b128c3a30d003c1a318a0612e4
Leul Zewdu <Leulzewduchanie@gmail.com>
2024-12-11 07:37:55 +0300

    formated the html and added new function

--- src/Stopwatch.jsx
+++ src/Stopwatch.jsx
@@ -2,19 +2,45 @@ import { useEffect, useRef, useState } from "react";
 
 const Stopwatch = () => {
 
+        const [time,setTime] = useState(0);
+        const [isRunning,setRunning] = useState(false);
+
+
+        const IntervalIdRef = useRef(null);
+
+        const startTimeRef = useRef(0);
+
+
+        useEffect(()=>{
+
+        },[isRunning])
 
         const stop = ()=>{
+
+        }
+        const start = ()=>{ 
+
            
+
         }
-        const start = ()=>{
-        }
+           
         const reset = ()=>{
-           
+          
+        }
+        const formatTime = ()=>{    
+
+                return `00:00:00`;
         }
     return ( 
-        <div>
-            <button onClick={stop} >Stop</button>
-            <button onClick={reset} >Reset</button>
+        <div className="stopwatch">
+            <div className="display">
+                {formatTime()}
+            </div>
+            <div className="controls">
+                <button onClick={start} className="start-btn">Start</button>
+                <button onClick={stop} className="stop-btn">Stop</button>
+                <button onClick={reset} className="reset-btn">Reset</button>
+            </div>
         </div>
      );
 }
